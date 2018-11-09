c5bd9b45a9662f93124e1e6f772bff7935400dc3
Robert Stein <robert@blueshoe.de>
2018-11-09 14:10:12 +0100

    - cleanup quickcheck - little sleep before before fetching data

--- quick_check.py
+++ quick_check.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from smartcard.CardRequest import CardRequest
 from smartcard.CardType import AnyCardType
-
+from time import sleep
 from healthcard.reader import HealthCardReader
 
-cardtype = AnyCardType()
-cardrequest = CardRequest( timeout=None, cardType=cardtype )
-cardservice = cardrequest.waitforcard()
-reader = HealthCardReader(cardservice=cardservice)
+
+reader = HealthCardReader()
+print('Trying to fetch card data...')
+sleep(0.5)
 healthcard = reader.get_health_card()
 print(healthcard.patient.to_json())
