aa55e060fd89106982306269e69c01e119116bce
Robert Stein <robert@blueshoe.de>
2018-11-09 18:30:58 +0100

    - adds setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='python-healthcard',
+      version='0.1',
+      description='A module to read german health insurance cards with python',
+      url='https://gitlab.blueshoe.de/Blueshoe/python-healthcard/',
+      author='Blueshoe',
+      author_email='robert@blueshoe.de',
+      license='MIT',
+      packages=['healthcard'],
+      zip_safe=False
+)
+
