7696609c1fdfb5c530d6c6015d0d6e8ee11fc42a
Robert Stein <robert@blueshoe.de>
2018-11-09 18:27:49 +0100

    - shorter interval

--- quick_check.py
+++ quick_check.py
@@ -4,6 +4,6 @@ from healthcard.reader import HealthCardReader
 
 reader = HealthCardReader()
 print('Trying to fetch card data...')
-sleep(0.5)
+sleep(0.2)
 healthcard = reader.get_health_card()
 print(healthcard.patient.to_json())
