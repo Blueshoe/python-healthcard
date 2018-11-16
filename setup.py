bf607d89fd4dfd85f13ee4d6dc4dc84d5eff92aa
Robert Stein <robert@blueshoe.de>
2018-11-16 12:50:09 +0100

    - bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='python-healthcard',
-      version='0.1',
+      version='0.1.1',
       description='A module to read german health insurance cards with python',
       url='https://gitlab.blueshoe.de/Blueshoe/python-healthcard/',
       author='Blueshoe',
