2c773368e0a7866d704f0c58e68176f0aa85aa61
Robert Stein <robert@blueshoe.de>
2021-05-01 18:29:25 +0200

    fix: setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+import pathlib
 from setuptools import setup
 from healthcard import __version__
+
+DIR = pathlib.Path(__file__).parent
+
+README = (DIR / "README.rst").read_text()
+
 
 setup(
     name="python-healthcard",
@@ -10,6 +16,8 @@ setup(
     author="Blueshoe",
     author_email="robert@blueshoe.de",
     license="MIT",
+    long_description=README,
+    long_description_content_type="text/x-rst",
     packages=["healthcard"],
     install_requires=["pyscard==1.9.6", "lxml==4.6.3"],
     zip_safe=False,
